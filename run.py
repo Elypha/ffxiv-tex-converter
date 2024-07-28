746ca224a5cbf65e644cbd6eb2f101320435a398
emar <49133270+emarron@users.noreply.github.com>
2024-07-27 20:47:02 -0400

    switching to a chunk based approach

--- run.py
+++ run.py
@@ -3,20 +3,21 @@ from pathlib import Path
 from src.packageland import handler
 from src.converters import dds_to_tex, tex_to_dds
 
-parser = argparse.ArgumentParser(
-    description="ffxiv-tex-converter, FFXIV TEX FILE CONVERTER")
-parser.add_argument('--directory', '-d', metavar='-D', type=str, help='Initial directory to be processed.',
+parser = argparse.ArgumentParser(description="ffxiv-tex-converter, FFXIV TEX FILE CONVERTER")
+parser.add_argument('--directory', '-d', metavar='-D', type=str, 
+                    help='Initial directory to be processed.',
                     required=True)
-parser.add_argument('--command', '-c', metavar='-C', type=str,
-                    help='dds-to-tex, tex-to-dds', required=True)
-parser.add_argument('--parallel', '-p', metavar='-P', action=argparse.BooleanOptionalAction,
-                    help='multicore processing')
-parser.add_argument('--multiplier', '-m', metavar='-M', type=float, default=5,
-                    help='if using multicore processing, job multiplier per core. default = 5')
+parser.add_argument('--command', '-c', metavar='-C', type=str, 
+                    help='dds-to-tex, tex-to-dds', 
+                    required=True)
+parser.add_argument('--parallel', '-p', action='store_true',
+                    help='Enable multicore processing')
+parser.add_argument('--chunk-size', '-cs', metavar='-CS', type=int, default=100,
+                    help='Target chunk size in MB for parallel processing. default = 100')
+
 args = parser.parse_args()
 
 folder = Path(args.directory)
-
 
 def read_command(command):
     if 'dds-to-tex' == command.lower():
@@ -48,6 +49,6 @@ if __name__ == '__main__':
     grabber = list(folder.glob('**/*.*'))
     function = read_command(args.command)
     if args.parallel:
-        handler.parallel_process(grabber, function, args.multiplier)
+        handler.parallel_process(grabber, function, args.chunk_size)
     else:
         handler.solo_process(grabber, function)
